e7d5f216f12ec9cbe9c525e4da40039d9d00a143
unknown <peter32parker31@gmail.com>
2023-04-03 13:19:53 +0530

    initial commit

---
+++ releational operator.c
@@ -0,0 +1,14 @@
+//logical opertors
+int main()
+{
+    int a=32;
+    printf("\nlogical and :%d",(a>=35 && a<=100));
+    printf("\nlogical or :%d",(a>=35 || a<=100));
+    printf("\n logical not : %d",(a>=35));
+    return 0;
+
+
+
+
+
+    }
